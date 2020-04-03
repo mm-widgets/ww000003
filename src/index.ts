681852cbd5b8bb96e75adf458a1b628f58083849
taoqf <tao_qiufeng@126.com>
2020-04-03 16:07:54 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,220 @@
-import on, { emit } from '@mmstudio/on';
+import on, { emit, once } from '@mmstudio/on';
+import Selectr, { IData } from 'mobius1-selectr';
 
 const no = 'mm-000003';
 
-/**
- * :host表示选中当前组件,使用all:initial;属性,
- * 当外部有css样式要越过shadowDom边界时将该css样式重置为初始值,从而不在影响shaowDom内部的样式.
- * 参考链接https://developers.google.cn/web/fundamentals/web-components/shadowdom#reset,
- * 使用 000003 作为所有样式类的前缀可以作为前置限定，从而避免某些浏览器对shadowdom支持不好，样式错乱的问题,
- * tpl 中可以添加link标签，但是最好使用公网可以访问到的cdn连接，且一定要保证版本号固定，如<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/jstree@3.3.7/dist/themes/default/style.min.css" />
- */
-const tpl = `
+const styles = `
 <style>
-.${no} {
-	width: 100%;
-	height: inherit;
-	overflow: hidden;
-	position: relative;
-}
 :host {
-	all: initial;
+	display:block;
 }
-</style>
-
-<div class="${no}">
-	<div id="n01">Hello <a href="https://mmstudio.gitee.io/">mmstudio</a></div>
-</div>
+</style>`;
+const tpl = `
+${styles}
+<select id='fd-w000018-mySelect'>
+	<slot id='fd-w000018-slot' ></slot>
+</select>
 `;
 
-export default class Widget extends HTMLElement {
+export type IDa = IDb | string;
+
+export interface IDb {
+	[field: string]: string | number | boolean | null | undefined;
+}
+
+interface SingleSelected {
+	text: string;
+	value: string;
+}
+
+interface MultiSelected {
+	values: SingleSelected[];
+}
+
+type Selected = SingleSelected | MultiSelected;
+
+
+export default class DropdownSelect extends HTMLElement {
+	private dom: ShadowRoot;
+	private selector!: Selectr;
+	// private children_node: Element[];
 	public constructor() {
 		super();
-		const dom = this.attachShadow({ mode: 'closed' });
+		const dom = this.dom = this.attachShadow({ mode: 'closed' });
 		dom.innerHTML = tpl;
-		const s = this.getAttribute('sattr');
-		const n = dom.querySelector<HTMLDivElement>('#n01')!;
-		on(n, 'click', () => {
-			emit(this, 'mmwe-click', true, true);
-		});
-		emit(this, 'mmwe-init', true, true, {
-			data: {
-				id: this.id
+		/**
+		 * slot.assignedElements 谷歌浏览器版本65一下不支持 详细可见[https://developer.mozilla.org/en-US/docs/Web/API/HTMLSlotElement/assignedElements]
+		 */
+		// const slot = dom.querySelector('#fd-w000018-slot') as HTMLSlotElement;
+		// this.children_node = (() => {
+		// 	if (slot.assignedElements) {
+		// 		return slot.assignedElements().filter((item) => {
+		// 			return item.localName === 'option' || item.localName === 'optgroup';
+		// 		});
+		// 	} else {
+		// 		return [];
+		// 	}
+		// })();
+		// this.children_node = Array.from(this.children) as Element[];
+		// this.innerHTML = tpl;
+		this.initialize();
+	}
+	public open() {
+		this.selector.open();
+	}
+	public close() {
+		this.selector.close();
+	}
+	public disable() {
+		this.selector.disable();
+	}
+	public enable() {
+		this.selector.enable();
+	}
+	public async render() {
+		// this.selector.render();
+	}
+	public set_data(data: IData | IData[]) {
+		this.selector.removeAll();
+		this.add_data(data);
+	}
+	public get_all_selected() {
+		const selected = this.selector.getValue(true) as Selected;
+		if (selected && (selected as MultiSelected).values) {
+			return (selected as MultiSelected).values;
+		}
+		return [selected as SingleSelected];
+
+	}
+
+	public set_selected(value: string) {
+		const old = this.get_all_selected();
+		if (old.length === 1 && old[0] && old[0].value === value) {
+			// 如果原来选中,再次调用选中时会取消选中
+			return;
+		}
+		this.selector.setValue(value);
+	}
+	public add_data(data: IData | IData[]) {
+		this.selector.add(data);
+	}
+
+	private del_data(data: IDa[]) {
+		const text_field = this.getAttribute('text-field') || 'text';
+		const value_field = this.getAttribute('value-field') || 'value';
+
+		const optgroup_text_field = this.getAttribute('optgroup-text-field') || 'text';
+		const optgroup_value_field = this.getAttribute('optgroup-value-field') || 'value';
+		return data.map((item) => {
+			if (typeof item === 'string') {
+				return {
+					text: item,
+					value: item
+				} as IData;
 			}
+			const children = ((item.children ? item.children : []) as IDb[]).map((item1) => {
+				return {
+					disabled: item1.disabled ? item1.disabled : false,
+					selected: item1.selected ? item1.selected : false,
+					text: item1[optgroup_text_field],
+					value: item1[optgroup_value_field] ? item1[optgroup_value_field] : item1[optgroup_text_field]
+				} as IData;
+			});
+			return {
+				children,
+				disabled: item.disabled ? item.disabled : false,
+				selected: item.selected ? item.selected : false,
+				text: item[text_field],
+				value: item[value_field] ? item[value_field] : item[text_field]
+			} as IData;
+
 		});
-		alert(`widget ${no} inited, attribte: sattr = ${s} `);
 	}
-	/**
-	 * 方法注释
-	 * @param param 参数注释
-	 */
-	public method01(param: string) {
-		window.console.debug('this is a public method', param);
-		this.method02(param);
+
+	private initialize() {
+		if (this.selector) {
+			return;
+		}
+		const dom = this.dom;
+		// 显示数据
+		const data_temp = this.getAttribute('data');
+		const data = (data_temp && this.del_data(JSON.parse(data_temp))) || undefined;
+		// 搜索框占位符(默认值)
+		const searchPlaceholder = this.getAttribute('searchPlaceholder') || '';
+		// 查不到结果显示的文字
+		const searchText = this.getAttribute('searchText') || '';
+		// 是否为多选
+		const multiple = this.hasAttribute('multiple');
+		// 允许在单个选择下拉列表中取消选择值
+		const allowDeselect = this.hasAttribute('allowDeselect');
+		// 搜索框
+		const searchable = this.hasAttribute('searchable');
+		const css = document.createElement('link');
+		css.setAttribute('rel', 'stylesheet');
+		css.setAttribute('type', 'text/css');
+		css.href = '//cdn.jsdelivr.net/npm/mobius1-selectr@2.4.13/dist/selectr.min.css';
+		dom.appendChild(css);
+		// 没有选择时默认显示的文本
+		const placeholder = this.getAttribute('placeholder') || '请选择...';
+		// 获取宽
+		const select = dom.querySelector<HTMLSelectElement>('#fd-w000018-mySelect')!;
+		const width = this.getAttribute('width') || this.style.width;
+		if (width !== null || width !== '') {
+			select.style.width = width;
+		}
+		if (this.children.length > 0) {
+			Array.from(this.children).filter((item) => {
+				return item.localName === 'option' || item.localName === 'optgroup';
+			}).forEach((child) => {
+				select.appendChild(child);
+			});
+		}
+		once(css, 'load', (() => {
+			this.selector = new Selectr(select, {
+				allowDeselect,
+				data,
+				messages: {
+					noResults: searchText,
+					searchPlaceholder
+				},
+				multiple,
+				placeholder,
+				searchable
+			});
+			this.add_event_listener();
+		}));
 	}
-	private method02(param: string) {
-		alert(`This is a private method: ${param}`);
+
+	private add_event_listener() {
+		this.selector.on('selectr.select', (res) => {
+			emit(this, 'fdwe-select', true, true, { text: res.text, value: res.value });
+		});
+		this.selector.on('selectr.deselect', (res) => {
+			emit(this, 'fdwe-select-deselect', true, true, { text: res.text, value: res.value });
+		});
+		this.selector.on('selectr.change', (res) => {
+			const params = res || {};
+			emit(this, 'fdwe-select-change', true, true, { text: params.text, value: params.value });
+		});
+		this.selector.on('selectr.open', () => {
+			emit(this, 'fdwe-select-open', true, true);
+		});
+		this.selector.on('selectr.close', () => {
+			emit(this, 'fdwe-select-close', true, true);
+		});
+		this.selector.on('selectr.clear', () => {
+			emit(this, 'fdwe-select-clear', true, true);
+		});
+		this.selector.on('selectr.reset', () => {
+			emit(this, 'fdwe-select-reset', true, true);
+		});
+		// 阻止事件冒泡,以免document上的click事件将下拉框关闭
+		on(this, 'click', (e) => {
+			e.stopPropagation();
+		});
 	}
 }
 
 if (!window.customElements.get(no)) {
-	window.customElements.define(no, Widget);
+	window.customElements.define(no, DropdownSelect);
 }
-
--- tests/test.ts
+++ tests/test.ts
@@ -1,12 +1,34 @@
 import Widget from '../src/index';
 import '../src/index';
 
-const widget = document.querySelector<Widget>('#widget')!;
-widget.addEventListener('mmwe-event', () => {
-	// console.info(e.data);
+const w1 = document.querySelector<Widget>('#w1')!;
+w1.addEventListener('fdwe-select-change', (e) => {
+	console.info('change event:', e);
+});
+const w2 = document.querySelector<Widget>('#w2')!;
+const w3 = document.querySelector<Widget>('#w3')!;
+const w4 = document.querySelector<Widget>('#w4')!;
+const w5 = document.querySelector<Widget>('#w5')!;
+const w6 = document.querySelector<Widget>('#w6')!;
+const btn = document.querySelector('#btn')!;
+const btn2 = document.querySelector('#btn2')!;
+const btn3 = document.querySelector('#btn3')!;
+btn.addEventListener('click', async () => {
+	w1.set_selected('CCCCCC');
+	w2.set_selected('CCCCCC');
+	console.log('11111111111111 ', await w2.get_all_selected());
+	console.log('get data1: ', await w1.get_all_selected());
+	console.log('get data2: ', await w2.get_all_selected());
+	console.log('get data3: ', await w3.get_all_selected());
+	console.log('get data4: ', await w4.get_all_selected());
+	console.log('get data5: ', await w5.get_all_selected());
+	console.log('get data6: ', await w6.get_all_selected());
 });
 
-const btn = document.querySelector('#test')!;
-btn.addEventListener('click', () => {
-	widget.method01('mmstudio');
+btn2.addEventListener('click', () => {
+	w2.disable();
+});
+
+btn3.addEventListener('click', () => {
+	w2.enable();
 });
